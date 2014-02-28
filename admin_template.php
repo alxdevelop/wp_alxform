18e4a67a4fe5e0e67dd60e3bcfa1c22160d03762
Alejandro Sanchez <alx.develop@gmail.com>
2014-02-27 23:10:39 -0600

    se agrego el template del admin

---
+++ admin_template.php
@@ -0,0 +1,23 @@
+<h1>AlxForm</h1>
+
+<p>Panel de administración del formulario de contacto</p>
+
+<form method="POST" action="">
+<h3>Email</h3>
+<p>Ingrese el e-mail donde llegaran todas las consultas</p>
+
+<input type="text" name="alxform_email" id="alxform_email" value="<?php echo $alxform_email ?>" />
+
+<h3>URL</h3>
+<p>Ingrese la url donde quiera que le reedirija despues de enviar la información</p>
+<input type="text" name="alxform_url" id="alxform_url" value="<?php echo $alxform_url ?>" />
+<br />
+<br />
+<button>Guardar</button>
+
+</form>
+
+<br />
+<br />
+<i>Desarrollado por @alxdevelop</i>
+
--- wp_alxform.php
+++ wp_alxform.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Plugin Name: wp_alxform
+Plugin Name: AlxForm
 Plugin URI:
 Description: Formulario de contacto
 Author: @alxdevelop
@@ -54,6 +54,26 @@ add_action('admin_menu',
 
 function wp_alxform_admin()
 {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "alxform_config";
+    $alxform_email = "";
+    $alxform_url = "";
+	if(count($_POST) > 0){
+
+    $alxform_email = trim($_POST['alxform_email']);
+    $alxform_url = trim($_POST['alxform_url']);
+    $wpdb->query("UPDATE $table_name SET email = '$alxform_email', url = '$alxform_url' WHERE id = 1");
+		/*$sf_email = $_POST['sf-email'];  
+		$sf_confirm = $_POST['sf-confirm'];  
+        update_option('simpleform-email', trim($sf_email));  
+    update_option('simpleform-confirmation-message', trim($sf_confirm));*/  
+		echo "<div class='updated'><p><strong>Configuración guardada</strong></p></div>";
+	}else {  
+    $rows = $wpdb->get_row("SELECT * FROM $table_name WHERE id = 1");
+    $alxform_email = $rows->email;
+    $alxform_url = $rows->url;
+    }  
+
   include("admin_template.php");
 }
 
